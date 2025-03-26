dea245a51edb97f16cd49160cc83a119aff8cb6b
a.busik <a.busik@iridi.tech>
2025-03-26 17:41:18 +0500

    2

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+    new fullpage('#fullpage', {
+        autoScrolling:true,
+        scrollHorizontally: false,
+        responsiveWidth: 1200,
+    });
+});
