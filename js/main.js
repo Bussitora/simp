4926409aec36df1370d7919e09b3195b9f98163d
Bussitora <bussitora@gmail.com>
2025-04-03 00:57:05 +0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', function () {
-    new fullpage('#fullpage', {
-        autoScrolling:true,
-        scrollHorizontally: false,
-        responsiveWidth: 1440,
-    });
+    // new fullpage('#fullpage', {
+    //     autoScrolling:true,
+    //     scrollHorizontally: false,
+    //     responsiveWidth: 1440,
+    // });
+    IMask(
+        document.getElementById('phone-mask'),
+        {
+          mask: '+{7} 000 000-00-00'
+        }
+      )
 });
